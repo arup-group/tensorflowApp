591cecb08af935f0aef1066b386e274225a52cf6
armstrong0919 <armstrong0919@gmail.com>
2024-04-29 16:32:38 +0800

    tensorflow model version

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ import * as cocossd from '@tensorflow-models/coco-ssd'
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import * as jpeg from 'jpeg-js';
-import { fetch, bundleResourceIO } from '@tensorflow/tfjs-react-native';
-import * as blazeface from '@tensorflow-models/blazeface';
-import Svg, {Rect} from 'react-native-svg';
-import * as Model from './utilits/modelLoader.js';
+// import { fetch, bundleResourceIO } from '@tensorflow/tfjs-react-native';
+// import * as blazeface from '@tensorflow-models/blazeface';
+// import Svg, {Rect} from 'react-native-svg';
+// import * as Model from './utilits/modelLoader.js';
 
 
 const App = () => {
@@ -27,7 +27,7 @@ const App = () => {
   const [result, setResult] = useState('');
   const [isEnabled,setIsEnabled] = useState(true)
   const [imageLink,setImageLink] = useState("https://raw.githubusercontent.com/ohyicong/masksdetection/master/dataset/without_mask/142.jpg")
-  const [pickedImage, setPickedImage] = useState("https://raw.githubusercontent.com/ohyicong/masksdetection/master/dataset/without_mask/142.jpg");
+  const [pickedImage, setPickedImage] = useState("");
   const [faces,setFaces]=useState([])
   const [faceDetector,setFaceDetector]=useState("")
   const [maskDetector,setMaskDetector]=useState("")
@@ -50,10 +50,10 @@ const App = () => {
     try {
       // Load mobilenet.
       await tf.ready();
-      const modelJson = await require("./assets/model/model.json");
-      const modelWeight = await require("./assets/model/group1-shard.bin");
-      const maskDetector = await tf.loadLayersModel(bundleResourceIO(modelJson,modelWeight));
-      // const model = await mobilenet.load();
+      // const modelJson = await require("./assets/model/model.json");
+      // const modelWeight = await require("./assets/model/group1-shard.bin");
+      // const maskDetector = await tf.loadLayersModel(bundleResourceIO(modelJson,modelWeight));
+      const model = await mobilenet.load();
       // const model = await Model.loadModel();
       setIsTfReady(true);
       console.log("starting inference with picked image: " + pickedImage)
@@ -79,99 +79,99 @@ const App = () => {
     }
   };
 
-  function imageToTensor(rawImageData){
-    //Function to convert jpeg image to tensors
-    const TO_UINT8ARRAY = true;
-    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
-    // Drop the alpha channel info for mobilenet
-    const buffer = new Uint8Array(width * height * 3);
-    let offset = 0; // offset into original data
-    for (let i = 0; i < buffer.length; i += 3) {
-      buffer[i] = data[offset];
-      buffer[i + 1] = data[offset + 1];
-      buffer[i + 2] = data[offset + 2];
-      offset += 4;
-    }
-    return tf.tensor3d(buffer, [height, width, 3]);
-  }
-  const getFaces = async() => {
-    try{
-      console.log("[+] Retrieving image from link :"+imageLink)
-      const response = await fetch(imageLink, {}, { isBinary: true });
-      const rawImageData = await response.arrayBuffer();
-      const imageTensor = imageToTensor(rawImageData).resizeBilinear([224,224])
-      const faces = await faceDetector.estimateFaces(imageTensor, false);
-      var tempArray=[]
-      //Loop through the available faces, check if the person is wearing a mask. 
-      for (let i=0;i<faces.length;i++){
-        let color = "red"
-        let width = parseInt((faces[i].bottomRight[1] - faces[i].topLeft[1]))
-        let height = parseInt((faces[i].bottomRight[0] - faces[i].topLeft[0]))
-        let faceTensor=imageTensor.slice([parseInt(faces[i].topLeft[1]),parseInt(faces[i].topLeft[0]),0],[width,height,3])
-        faceTensor = faceTensor.resizeBilinear([224,224]).reshape([1,224,224,3])
-        let result = await maskDetector.predict(faceTensor).data()
-        //if result[0]>result[1], the person is wearing a mask
-        if(result[0]>result[1]){
-          color="green"
-        }
-        tempArray.push({
-          id:i,
-          location:faces[i],
-          color:color
-        })
-      }
-      setFaces(tempArray)
-      console.log("[+] Prediction Completed")
-    }catch(error){
-      console.log("[-] Unable to load image")
-      console.log(error)
-    }
+  // function imageToTensor(rawImageData){
+  //   //Function to convert jpeg image to tensors
+  //   const TO_UINT8ARRAY = true;
+  //   const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
+  //   // Drop the alpha channel info for mobilenet
+  //   const buffer = new Uint8Array(width * height * 3);
+  //   let offset = 0; // offset into original data
+  //   for (let i = 0; i < buffer.length; i += 3) {
+  //     buffer[i] = data[offset];
+  //     buffer[i + 1] = data[offset + 1];
+  //     buffer[i + 2] = data[offset + 2];
+  //     offset += 4;
+  //   }
+  //   return tf.tensor3d(buffer, [height, width, 3]);
+  // }
+  // const getFaces = async() => {
+  //   try{
+  //     console.log("[+] Retrieving image from link :"+imageLink)
+  //     const response = await fetch(imageLink, {}, { isBinary: true });
+  //     const rawImageData = await response.arrayBuffer();
+  //     const imageTensor = imageToTensor(rawImageData).resizeBilinear([224,224])
+  //     const faces = await faceDetector.estimateFaces(imageTensor, false);
+  //     var tempArray=[]
+  //     //Loop through the available faces, check if the person is wearing a mask. 
+  //     for (let i=0;i<faces.length;i++){
+  //       let color = "red"
+  //       let width = parseInt((faces[i].bottomRight[1] - faces[i].topLeft[1]))
+  //       let height = parseInt((faces[i].bottomRight[0] - faces[i].topLeft[0]))
+  //       let faceTensor=imageTensor.slice([parseInt(faces[i].topLeft[1]),parseInt(faces[i].topLeft[0]),0],[width,height,3])
+  //       faceTensor = faceTensor.resizeBilinear([224,224]).reshape([1,224,224,3])
+  //       let result = await maskDetector.predict(faceTensor).data()
+  //       //if result[0]>result[1], the person is wearing a mask
+  //       if(result[0]>result[1]){
+  //         color="green"
+  //       }
+  //       tempArray.push({
+  //         id:i,
+  //         location:faces[i],
+  //         color:color
+  //       })
+  //     }
+  //     setFaces(tempArray)
+  //     console.log("[+] Prediction Completed")
+  //   }catch(error){
+  //     console.log("[-] Unable to load image")
+  //     console.log(error)
+  //   }
     
-  }
-  // useEffect(() => {
-  //   classifyUsingMobilenet()
-  // }, [pickedImage]);
+  // }
   useEffect(() => {
-    async function loadModel(){
-      //Wait for tensorflow module to be ready
+    classifyUsingMobilenet()
+  }, [pickedImage]);
+  // useEffect(() => {
+  //   async function loadModel(){
+  //     //Wait for tensorflow module to be ready
 
-      class L2 {
+  //     class L2 {
 
-        static className = 'L2';
+  //       static className = 'L2';
     
-        constructor(config) {
-           return tf.regularizers.l1l2(config)
-        }
-    }
-    tf.serialization.registerClass(L2);
-      console.log("[+] Application started")
-      const tfReady = await tf.ready();
+  //       constructor(config) {
+  //          return tf.regularizers.l1l2(config)
+  //       }
+  //   }
+  //   tf.serialization.registerClass(L2);
+  //     console.log("[+] Application started")
+  //     const tfReady = await tf.ready();
 
-      //Replce model.json and group1-shard.bin with your own custom model
-      console.log("[+] Loading custom mask detection model")
-      const modelJson = await require("./assets/model/model.json");
-      console.log("[+] Loading modelJson")
-      const modelWeight = await require("./assets/model/group1-shard1of1.bin");
-      console.log("[+] Loading modelWeight")
-      try{
-        const maskDetector = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeight));
-      }catch(error){
-        console.log("model : ",error )
-      }
+  //     //Replce model.json and group1-shard.bin with your own custom model
+  //     console.log("[+] Loading custom mask detection model")
+  //     const modelJson = await require("./assets/model/model.json");
+  //     console.log("[+] Loading modelJson")
+  //     const modelWeight = await require("./assets/model/group1-shard1of1.bin");
+  //     console.log("[+] Loading modelWeight")
+  //     try{
+  //       const maskDetector = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeight));
+  //     }catch(error){
+  //       console.log("model : ",error )
+  //     }
       
-      console.log("[+] Loading pre-trained face detection model")
-      // //Blazeface is a face detection model provided by Google
+  //     console.log("[+] Loading pre-trained face detection model")
+  //     // //Blazeface is a face detection model provided by Google
 
-      const faceDetector =  await blazeface.load();
-      console.log("[+] faceDetector")
+  //     const faceDetector =  await blazeface.load();
+  //     console.log("[+] faceDetector")
 
-      // //Assign model to variable
-      setMaskDetector(maskDetector)
-      setFaceDetector(faceDetector)
-      // console.log("[+] Model Loaded")
-    }
-    loadModel()
-  }, []); 
+  //     // //Assign model to variable
+  //     setMaskDetector(maskDetector)
+  //     setFaceDetector(faceDetector)
+  //     // console.log("[+] Model Loaded")
+  //   }
+  //   loadModel()
+  // }, []); 
   return (
   //   <View style={styles.container}>
   //   <Input 
@@ -245,10 +245,10 @@ const App = () => {
       {!isTfReady && <Text>Loading TFJS model...</Text>}
       {isTfReady && result === '' && <Text>Pick an image to classify!</Text>}
       {result !== '' && <Text>{result}</Text>}
-      <Button 
+      {/* <Button 
           title="Predict"
           onPress={()=>{getFaces()}}
-        />
+        /> */}
     </View>
   );
 };
